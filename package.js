f4eb82a9dff6e74b82b7d3ac980a0ea16d329456
Patrick Lewis <patrickml.pml@gmail.com>
2016-06-14 09:11:21 -0400

    Updated Docs

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'patrickml:event-horizon',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'flux the meteor way',
   // URL to the Git repository containing the source code for this package.
